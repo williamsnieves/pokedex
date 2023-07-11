75d2e27c6f8f61c6bedbe5fca10a689416e8c15b
Willians Nieves <williansnieves@Willianss-MBP.home>
2023-07-11 13:27:58 +0200

    test: added vitest config

---
+++ react-version/src/components/Pokemon.test.ts
@@ -0,0 +1,5 @@
+import { expect, test } from "vitest";
+
+test("Pokemon", () => {
+  expect(1).toBe(1);
+});
---
+++ react-version/vitest.config.ts
@@ -0,0 +1,30 @@
+import path from "node:path";
+import { defineConfig, defaultExclude } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    setupFiles: path.resolve(__dirname, "./test/setup.ts"),
+    exclude: [...defaultExclude, "**/node_modules/**"],
+    environmentMatchGlobs: [
+      ["**/*.test.tsx", "jsdom"],
+      ["**/*.component.test.ts", "jsdom"],
+    ],
+    coverage: {
+      statements: 54.92,
+      thresholdAutoUpdate: true,
+      include: ["srcs/**/*"],
+      exclude: [
+        "test/**",
+        "vite.*.ts",
+        "**/*.d.ts",
+        "**/*.test.*",
+        "**/*.config.*",
+        "**/snapshot-tests/**",
+        "**/*.solution.tsx",
+        "**/coverage/**",
+      ],
+      all: true,
+    },
+  },
+});
